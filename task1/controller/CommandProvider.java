628eed2aa50f768a780cac55f7c04ec927f00d9f
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-05-12 20:02:59 +0200

    Create CommandProvider.java

---
+++ task1/controller/CommandProvider.java
@@ -0,0 +1,25 @@
+package by.epam.tasks.task1.controller;
+
+import java.util.*;
+import by.epam.tasks.task1.controller.impl.LoginationCommand;
+import by.epam.tasks.task1.controller.impl.RegistrationCommand;
+import by.epam.tasks.task1.controller.impl.ViewLibraryCommand;
+
+public class CommandProvider {
+
+	private Map<String, Command> commands = new HashMap<>();
+
+	public CommandProvider() {
+		super();
+		commands.put("view_library", new ViewLibraryCommand());
+		commands.put("logination", new LoginationCommand());
+		commands.put("registration", new RegistrationCommand());
+	}
+
+	public Command getCommand(String commandName) {
+		Command command;
+		command = commands.get(commandName);
+		return command;
+	}
+
+}
