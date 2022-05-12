83d6d087e383d904b46638664c32687b17e7eba7
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-05-12 19:57:41 +0200

    Create Book.java

---
+++ task1/bean/Book.java
@@ -0,0 +1,108 @@
+package by.epam.tasks.task1.bean;
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+    private static int ID = 0;
+    private int id;
+    private String title;
+    private String author;
+    private String publisher;
+    private int year;
+    
+    public Book(String title, String author, String publisher, int year) {
+        this.id = ++ID;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.year = year;
+    }
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((publisher == null) ? 0 : publisher.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + year;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (id != other.id)
+			return false;
+		if (publisher == null) {
+			if (other.publisher != null)
+				return false;
+		} else if (!publisher.equals(other.publisher))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (year != other.year)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName() + " [id=" + id + ", title=" + title + ", author=" + author + ", publisher=" + publisher + ", year="
+				+ year + "]";
+	}
+
+}
